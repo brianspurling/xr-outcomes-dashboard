97273b46e1218ba3a4d588f4924b085d06145c9f
brianspurling <brian.spurling@gmail.com>
2020-03-22 13:25:03 +0000

    Update django.py

--- outcomes/outcomes/settings/django.py
+++ outcomes/outcomes/settings/django.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'outcomes.apps.dashboard',
+    'outcomes.outcomes.apps.dashboard',
 ]
 
 TEMPLATES = [

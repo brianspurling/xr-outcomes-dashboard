5bd5a50229cfb312f4b1af8ca205badc2668303f
brianspurling <brian.spurling@gmail.com>
2020-03-22 13:00:13 +0000

    Update urls.py

--- outcomes/outcomes/urls.py
+++ outcomes/outcomes/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from outcomes.outcomes.apps.dashboard import views
+from .apps.dashboard import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
